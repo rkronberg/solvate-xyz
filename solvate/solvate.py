a95d12941858030e4ed174c217ecd68b9d9dd094
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-24 16:46:12 +0300

    Split code

--- solvate/solvate.py
+++ solvate/solvate.py
@@ -11,42 +11,7 @@ email: rasmus.kronberg@aalto.fi
 import sys, os
 import numpy as np
 from ase.io import read, write
-
-def unitv(vector):
-
-	# Returns the unit vector
-	return vector/np.linalg.norm(vector)
-
-def angle(u, v):
-	
-	# Returns the angle in degrees between vectors u and v
-	uhat = unitv(u)
-	vhat = unitv(v)
-	return 360/(2*np.pi)*np.arccos(np.clip(np.dot(uhat, vhat), -1.0, 1.0))
-
-def cleanup(file, elems):
-
-	# Cleanup .xyz so that programs such as ase-gui understands the content
-	# Fix header
-	os.system("sed -i 's/Properties.*//' %s" % file)
-
-	# Strip everything but symbols and coordinates
-	outfile = '%s-solvated.xyz' % sys.argv[1].strip('.xyz')
-	with open(outfile, 'w') as out:
-		with open(file, 'r') as f:
-			line = f.readline()
-			while line != '':
-				tmp = line.strip().split()
-				if any(sym == tmp[0] for sym in elems):
-					string = '%3s %10.5f %10.5f %10.5f \n' %(tmp[0], float(tmp[1]), \
-					float(tmp[2]), float(tmp[3]))
-					out.write(string)
-				else:
-					out.write(line)
-				line = f.readline()
-
-	os.system('rm -f tmp.pdb *tmp.gro* *tmp_solv.pdb* tmp_solv.xyz')
-	print('Solvated structure written in %s' % outfile)
+from utils import angle, pbc, cleanup
 
 def main():
 
@@ -84,24 +49,14 @@ def main():
 	elems = set(solv.get_chemical_symbols())
 
 	# Wrap atoms in fractional coordinate space without breaking water molecules
-	for atom in solv:
-		# Original atoms and water oxygens
-		if atom.index < len(xyz) or atom.symbol == 'O':
-			frac = np.matmul(np.linalg.inv(cell.T), atom.position)
-			frac = (frac + 1.0) % 1.0
-			cart = np.matmul(cell.T, frac)
-			shift = cart - atom.position
-			atom.position = cart
-		# Shift water hydrogens accordingly
-		if atom.index >= len(xyz) and atom.symbol == 'O':
-			solv[atom.index + 1].position += shift
-			solv[atom.index + 2].position += shift
+	solv = pbc(xyz, solv, cell)
 
 	# Convert back to .xyz
 	write('tmp_solv.xyz', solv)
 
 	# Cleanup
-	cleanup('tmp_solv.xyz', elems)
+	outfile = cleanup('tmp_solv.xyz', elems)
+	print('Solvated structure written in %s' % outfile)
 	
 if __name__ == '__main__':
 	main()
---
+++ solvate/utils.py
@@ -0,0 +1,65 @@
+'''
+Utility functions for solvate.py
+
+author: Rasmus Kronberg
+email: rasmus.kronberg@aalto.fi
+'''
+
+import sys, os
+import numpy as np
+from ase.io import write
+
+def unitv(vector):
+
+	# Returns the unit vector
+	return vector/np.linalg.norm(vector)
+
+def angle(u, v):
+	
+	# Returns the angle in degrees between vectors u and v
+	uhat = unitv(u)
+	vhat = unitv(v)
+	return 360/(2*np.pi)*np.arccos(np.clip(np.dot(uhat, vhat), -1.0, 1.0))
+
+def pbc(xyz, solv, cell):
+
+	for atom in solv:
+		# Original atoms and water oxygens
+		if atom.index < len(xyz) or atom.symbol == 'O':
+			frac = np.matmul(np.linalg.inv(cell.T), atom.position)
+			frac = (frac + 1.0) % 1.0
+			cart = np.matmul(cell.T, frac)
+			shift = cart - atom.position
+			atom.position = cart
+		# Shift water hydrogens accordingly
+		if atom.index >= len(xyz) and atom.symbol == 'O':
+			solv[atom.index + 1].position += shift
+			solv[atom.index + 2].position += shift
+
+	return solv
+
+def cleanup(file, elems):
+
+	# Cleanup .xyz so that programs such as ase-gui understands the content
+	# Fix header
+	os.system("sed -i 's/Properties.*//' %s" % file)
+
+	# Strip everything but symbols and coordinates
+	outfile = '%s-solvated.xyz' % sys.argv[1].strip('.xyz')
+	with open(outfile, 'w') as out:
+		with open(file, 'r') as f:
+			line = f.readline()
+			while line != '':
+				tmp = line.strip().split()
+				if any(sym == tmp[0] for sym in elems):
+					string = '%3s %10.5f %10.5f %10.5f \n' %(tmp[0], float(tmp[1]), \
+					float(tmp[2]), float(tmp[3]))
+					out.write(string)
+				else:
+					out.write(line)
+				line = f.readline()
+
+	os.system('rm -f tmp.pdb *tmp.gro* *tmp_solv.pdb* tmp_solv.xyz')
+
+	return outfile
+	
